2756490d58e373603c1946f8d61701234c430aef
João Otávio Eccel <eccel511@gmail.com>
2023-07-09 21:51:29 -0300

    escopo 2

--- app.js
+++ app.js
@@ -6,5 +6,18 @@ function carregar() {
     var hora = data.getHours()
     var minuto = data.getMinutes() 
 
-    msg.innerHTML = `Agora são ${hora}:${minuto}h`
+    msg.innerHTML = `Agora são <strong>${hora}:${minuto}h</strong>.`
+
+    if (hora >= 4 && hora < 11) {
+        img.src = 'manha.jpg'
+    } else if (hora >= 11 && hora < 16) {
+        img.src = 'tarde.png'
+    } else if (hora >= 16 && hora < 18) {
+        img.src = 'entardecer.jpg'
+    } else if (hora >= 18 && hora < 4) {
+        img.src = 'noite.jpg'
+    } else {
+        window.alert('Erro, nunca deveria ter chegado aqui!')
+    }
+
 }
